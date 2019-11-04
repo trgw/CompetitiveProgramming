5b08b3122a8f6c3765b967723fb8f9105f2b9f5d
trgw <tregawa26@gmail.com>
2019-11-05 01:25:58 +0900

    2019/11/04

---
+++ AtCoder/Ash/Ashm/G40A.py
@@ -0,0 +1,68 @@
+'''ls = list(input())
+n = len(ls) + 1
+nm = ls.count('<')
+np = ls.count('>')
+la = [len(ls)]
+for i in range(n-1):
+    if ls[i] == '<':
+        la.append(la[i]+1)
+    else:
+        la.append(la[i]-1)
+print(la)
+print(sum(la) - n*min(la))
+print(sum(la))
+print(n*min(la))'''
+
+'''ls = list(input())
+n = len(ls) + 1
+la = [len(ls)]
+for i in range(n-1):
+    if ls[i] == '<' and ls[i+1] == '<':
+        la.append(la[i]+1)
+    elif ls[i] == '<' and ls[i+1] == '>':'''
+
+'''
+
+TLE
+
+s = input()
+n = len(s) + 1
+lft = 0
+rgt = 0
+l = []
+for i in range(n):
+    for j in reversed(range(i)):
+        if s[j] == '<':
+            lft += 1
+        else:
+            break
+    for j in range(i, n-1):
+        if s[j] == '>':
+            rgt += 1
+        else:
+            break
+    l.append(max(lft, rgt))
+    lft = 0
+    rgt = 0
+print(sum(l))'''
+
+s = input()
+n = len(s) + 1
+lft = 0
+rgt = 0
+l = []
+for i in range(n):
+    for j in reversed(range(i)):
+        if s[j] == '<':
+            lft += 1
+        else:
+            break
+    for j in range(i, n-1):
+        if s[j] == '>':
+            rgt += 1
+        else:
+            break
+    l.append(max(lft, rgt))
+    lft = 0
+    rgt = 0
+print(sum(l))
--- AtCoder/Test.py
+++ AtCoder/Test.py
@@ -1,19 +1,2 @@
-'''
-import sympy
-n = 10
-print(sympy.factor(n))
-'''
-
-# O(√n)enumerate_factors
-n = int(input())
-ln = []
-for i in range(1,(int(n**0.5))+1):
-    if n % i == 0:
-        ln.append(i)
-if not (n**0.5) % 1 == 0:
-    for i in reversed(range(0, len(ln))):
-        ln.append(n // ln[i])
-else:
-    for i in reversed(range(0, len(ln)-1)):
-        ln.append(n // ln[i])
-print(ln)
+s = input()
+print(5.count('<'))
